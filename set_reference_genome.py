5a3d07ae34cc5b1012df5d2a5a37d2fa7a0435f9
Haghani <vhaghani@ucdavis.edu>
2021-07-29 16:16:26 -0700

    write script for genome delineation

---
+++ set_reference_genome.py
@@ -0,0 +1,826 @@
+#!/usr/bin/env python3
+
+import os
+
+reference_genomes = {
+    "vicPac2": ["Alpaca", "link", "output"]
+}
+
+
+
+
+
+
+
+
+# Idea: put all the genome processing steps in a python file, run the python file (one rule) and have the output be a directory so it isn't specific to contents in the directory and makes the big snakefile look cleaner
+## Vertebrate genomes
+
+# Alpaca genome: Mar. 2013 (Vicugna_pacos-2.0.1/vicPac2)
+elif ref_genome == ""
+
+# Alpaca genome: Jul. 2008 (Broad/vicPac1)
+elif ref_genome == ""
+
+# African clawed frog: Aug. 2016 (Xenopus_laevis_v2)/xenLae2)
+elif ref_genome == ""
+
+# American alligator: Aug. 2012 (allMis0.2/allMis1)
+elif ref_genome == ""
+
+# Armadillo genome: Dec. 2011 (Baylor/dasNov3)
+elif ref_genome == ""
+
+# Atlantic cod genome: May 2010 (Genofisk GadMor_May2010/gadMor1)
+elif ref_genome == ""
+
+# Baboon genome: Apr. 2017 (Panu_3.0/papAnu4)
+elif ref_genome == ""
+
+# Baboon genome: Mar. 2012 (Baylor Panu_2.0/papAnu2)
+elif ref_genome == ""
+
+# Baboon genome: Nov. 2008 (Baylor Pham_1.0/papHam1)
+elif ref_genome == ""
+
+# Bison genome: Oct. 2014 (Bison_UMD1.0/bisBis1)
+elif ref_genome == ""
+
+# Bonobo genome: May 2020 (Mhudiblu_PPA_v0/panPan3)
+elif ref_genome == ""
+
+# Bonobo genome: Aug. 2015 (MPI-EVA panpan1.1/panPan2)
+elif ref_genome == ""
+
+# Bonobo genome: May 2012 (Max-Planck/panPan1)
+elif ref_genome == ""
+
+# Brown kiwi genome: Jun. 2015 (MPI-EVA AptMant0/aptMan1)
+elif ref_genome == ""
+
+# Budgerigar Genome: Sep. 2011 (WUSTL v6.3/melUnd1)
+elif ref_genome == ""
+
+# Bushbaby genome: Mar. 2011 (Broad/otoGar3)
+elif ref_genome == ""
+
+# Cat genome: Nov. 2017 (Felis_catus_9.0/felCat9)
+elif ref_genome == ""
+
+# Cat genome: Nov. 2014 (ICGSC Felis_catus_8.0/felCat8)
+elif ref_genome == ""
+
+# Cat genome: Sep. 2011 (ICGSC Felis_catus 6.2/felCat5)
+elif ref_genome == ""
+
+# Cat genome: Dec. 2008 (NHGRI/GTB V17/felCat4)
+elif ref_genome == ""
+
+# Cat genome: Mar. 2006 (Broad/felCat3)
+elif ref_genome == ""
+
+# Chicken genome: Mar. 2018 (Gallus_gallus-6.0/galGal6)
+elif ref_genome == ""
+
+# Chicken genome: Dec. 2015 (Gallus_gallus-5.0/galGal5)
+elif ref_genome == ""
+
+# Chicken genome: Nov. 2011 (ICGSC Gallus_gallus-4.0/galGal4)
+elif ref_genome == ""
+
+# Chicken genome: May 2006 ((WUGSC 2.1/galGal3)
+elif ref_genome == ""
+
+# Chicken genome: Feb. 2004 (WUGSC 1.0/galGal2)
+elif ref_genome == ""
+
+# Chimpanzee genome: Jan. 2018 (Clint_PTRv2/panTro6)
+elif ref_genome == ""
+
+# Chimpanzee genome: May. 2016 (Pan_tro 3.0/panTro5)
+elif ref_genome == ""
+
+# Chimpanzee genome: Feb. 2011 (SAC 2.1.4/panTro4
+elif ref_genome == ""
+
+# Chimpanzee genome: Oct. 2010 (CGSC 2.1.3/panTro3)
+elif ref_genome == ""
+
+# Chimpanzee genome: Mar. 2006 (CGSC 2.1/panTro2)
+elif ref_genome == ""
+
+# Chimpanzee genome: Nov. 2003 (CGSC 1.1/panTro1)
+elif ref_genome == ""
+
+# Chinese hamster genome: Jun. 2017 (CHOK1S_HZDv1/criGriChoV2)
+elif ref_genome == ""
+
+# Chinese hamster genome: Jul. 2013 (C_griseus_v1.0/criGri1)
+elif ref_genome == ""
+
+# Chinese hamster genome: Aug. 2011 (CriGri_1.0/criGriChoV1)
+elif ref_genome == ""
+
+# Chinese pangolin genome: Aug. 2014 (M_pentadactyla-1.1.1/manPen1)
+elif ref_genome == ""
+
+# Coelocanth genome: Aug. 2011 (LatCha1/latCha1)
+elif ref_genome == ""
+
+# Cow genome: Apr. 2018 (ARS-UCD1.2/bosTau9)
+elif ref_genome == ""
+
+# Cow genome: Jun. 2014 (UMD_3.1.1/bosTau8)
+elif ref_genome == ""
+
+# Cow genome: Oct. 2011 (Btau_4.6.1/bosTau7)
+elif ref_genome == ""
+
+# Cow genome: Nov. 2009 (Bos_taurus_UMD_3.1/bosTau6)
+elif ref_genome == ""
+
+# Cow genome: Oct. 2007 (Baylor 4.0/bosTau4)
+elif ref_genome == ""
+
+# Cow genome: Aug. 2006 (Baylor 3.1/bosTau3)
+elif ref_genome == ""
+
+# Cow genome: Mar. 2005 (Baylor 2.0/bosTau2)
+elif ref_genome == ""
+
+# Cow genome: May 2004 (bosTau1)
+elif ref_genome == ""
+
+# Crab-eating macaque genome: Jun. 2013 (Macaca_fascicularis_5.0/macFas5)
+elif ref_genome == ""
+
+# Dog genome: May 2019 (UMICH_Zoey_3.1/canFam5)
+elif ref_genome == ""
+
+# Dog genome: Mar. 2020 (UU_Cfam_GSD_1.0/canFam4)
+elif ref_genome == ""
+
+# Dog genome: Sep. 2011 (Broad CanFam3.1/canFam3)
+elif ref_genome == ""
+
+# Dog genome: May 2005 (Broad/canFam2)
+elif ref_genome == ""
+
+# Dog genome: Jul. 2004 (Broad/canFam1)
+elif ref_genome == ""
+
+# Dolphin genome: Oct. 2011 (Baylor Ttru_1.4/turTru2)
+elif ref_genome == ""
+
+# Elephant genome: Jul. 2009 (Broad/loxAfr3)
+elif ref_genome == ""
+
+# Elephant genome: May 2005 (loxAfr1/loxAfr1)
+elif ref_genome == ""
+
+# Elephant shark genome: Dec. 2013 (Callorhinchus_milii-6.1.3/calMil1)
+elif ref_genome == ""
+
+# Ferret genome: Apr. 2011 (MusPutFur1.0/musFur1)
+elif ref_genome == ""
+
+# Fugu genome: Oct. 2011 (fugu5/fr3)
+elif ref_genome == ""
+
+# Fugu genome: Oct. 2004 (JGI V4.0/fr2)
+elif ref_genome == ""
+
+# Fugu genome: Aug. 2002 (fugu/fr1)
+elif ref_genome == ""
+
+# Gibbon genome: Oct. 2012 (Nleu_3.0/nomLeu3)
+elif ref_genome == ""
+
+# Gibbon genome: Jun. 2011 (iGGSC Nleu1.1/nomLeu2)
+elif ref_genome == ""
+
+# Gibbon genome: Jan. 2010 (GGSC Nleu1.0/nomLeu1)
+elif ref_genome == ""
+
+# Garter snake genome: Jun. 2015 (Thanmnophis_sirtalis-6.0/thaSir1)
+elif ref_genome == ""
+
+# Golden eagle genome: Oct. 2014 (aquChr-1.0.2/aquChr2)
+elif ref_genome == ""
+
+# Golden snub-nosed monkey genome: Oct. 2014 (Rrox_v1/rhiRox1)
+elif ref_genome == ""
+
+# Gorilla genome: Aug. 2019 (Kamilah_GGO_v0/gorGor6)
+elif ref_genome == ""
+
+# Gorilla genome: Mar. 2016 (GMSRT3/gorGor5)
+elif ref_genome == ""
+
+# Gorilla genome: Dec 2014 (gorGor4.1/gorGor4)
+elif ref_genome == ""
+
+# Gorilla genome: May 2010 (gorGor3.1/gorGor3)
+elif ref_genome == ""
+
+# Green monkey genome: Mar 2014 (Chlorocebus_sabeus 1.1/chlSab2)
+elif ref_genome == ""
+
+# Guinea pig genome: Feb. 2008 (Broad/cavPor3)
+elif ref_genome == ""
+
+# Hawaiian monk seal genome: Jun. 2017 (ASM220157v1/neoSch1)
+elif ref_genome == ""
+
+# Hedgehog genome: May 2012 (EriEur2.0/eriEur2)
+elif ref_genome == ""
+
+# Hedgehog genome: Jun. 2006 (Broad/eriEur1)
+elif ref_genome == ""
+
+# Horse genome: Jan. 2018 (EquCab3.0/equCab3)
+elif ref_genome == ""
+
+# Horse genome: Sep. 2007 (Broad/equCab2)
+elif ref_genome == ""
+
+# Horse genome: Jan. 2007 (Broad/equCab1)
+elif ref_genome == ""
+
+# Human genome: Dec. 2013 (GRCh38/hg38)
+elif ref_genome == "hg38"
+
+# Human genome: Feb. 2009 (GRCh37/hg19)
+elif ref_genome == "hg19"
+
+# Kangaroo rat genome: Jul. 2008 (Broad/dipOrd1)
+elif ref_genome == ""
+
+# Lamprey genome: Dec. 2017 (Pmar_germline 1.0/petMar3)
+elif ref_genome == ""
+
+# Lamprey genome: Sep. 2010 (WUGSC 7.0/petMar2)
+elif ref_genome == ""
+
+# Lamprey genome: Mar. 2007 (WUGSC 3.0/petMar1)
+elif ref_genome == ""
+
+# Lancelet genome: Mar. 2006 (JGI 1.0/braFlo1)
+elif ref_genome == ""
+
+# Lizard genome: May 2010 (Broad AnoCar2.0/anoCar2)
+elif ref_genome == ""
+
+# Lizard genome: Jan. 2007 (Broad/anoCar1)
+elif ref_genome == ""
+
+# Malayan flying lemur genome: Jun. 2014 (G_variegatus-3.0.2/galVar1)
+elif ref_genome == ""
+
+# Manatee genome: Oct. 2011 (Broad v1.0/triMan1)
+elif ref_genome == ""
+
+# Marmoset genome: May 2020 (Callithrix_jacchus_cj1700_1.1/calJac4)
+elif ref_genome == ""
+
+# Marmoset genome: Mar. 2009 (WUGSC 3.2/calJac3)
+elif ref_genome == ""
+
+# Marmoset genome: Jun. 2007 (WUGSC 2.0.2/calJac1)
+elif ref_genome == ""
+
+# Medaka genome: Oct. 2005 (NIG/UT MEDAKA1/oryLat2)
+elif ref_genome == ""
+
+# Medium ground finch genome: Apr. 2012 (GeoFor_1.0/geoFor1)
+elif ref_genome == ""
+
+# Megabat genome: Jul. 2008 (Broad/pteVam1)
+elif ref_genome == ""
+
+# Microbat genome: Jul. 2010 (Myoluc2.0/myoLuc2)
+elif ref_genome == ""
+
+# Minke whale genome: Oct. 2013 (KORDI BalAcu1.0/balAcu1)
+elif ref_genome == ""
+
+# Mouse genome: Jun. 2020 (GRCm39/mm39)
+elif ref_genome == "mm39"    
+    rule set_reference_genome:
+        message: "Preparing reference genome for alignment" 
+        conda: "00_conda_software/chip_bwa.yml"
+        input: "mm39_ref_genome"
+        output: multiext("01_raw_data/mm39", ".amb", ".ann", ".bwt", ".pac", ".sa")
+        log: "00_logs/set_mm39_reference_genome.log"
+        shell: "snakemake -s {input} -j 1 -p"
+
+# Mouse genome: Dec. 2011 (GRCm38/mm10)
+elif ref_genome == ""
+
+# Mouse genome: Jul. 2007 (NCBI37/mm9)
+elif ref_genome == ""
+
+# Mouse genome: Mar. 2006 (NCBI36/mm8)
+elif ref_genome == ""
+
+# Mouse genome: Aug. 2005 (NCBI35/mm7)
+elif ref_genome == ""
+
+# Mouse genome: Mar. 2005 (mm6)
+elif ref_genome == ""
+
+# Mouse genome: May 2004 (mm5)
+elif ref_genome == ""
+
+# Mouse genome: Oct. 2003 (mm4)
+elif ref_genome == ""
+
+# Mouse genome: Feb. 2003 (mm3)
+elif ref_genome == ""
+
+# Mouse genome: Feb. 2002 (mm2)
+elif ref_genome == ""
+
+# Mouse genome: Nov. 2001 (mm1)
+elif ref_genome == ""
+
+# Mouse lemur genome: May 2015 (Mouse lemur/micMur2)
+elif ref_genome == ""
+
+# Mouse lemur genome: Jul. 2007 (Broad/micMur1)
+elif ref_genome == ""
+
+# Naked mole-rat genome: Jan. 2012 (Broad HetGla_female_1.0/hetGla2)
+elif ref_genome == ""
+
+# Naked mole-rat genome: Jul. 2011 (HetGla_1.0/hetGla1)
+elif ref_genome == ""
+
+# Nile tilapia genome: Jan. 2011 (Nile tilapia/oreNil2)
+elif ref_genome == ""
+
+# Opossum genome: Oct. 2006 (Broad/monDom5)
+elif ref_genome == ""
+
+# Opossum genome: Jan. 2006 (Broad/monDom4)
+elif ref_genome == ""
+
+# Opossum genome: Jun. 2005 (Broad/monDom2)
+elif ref_genome == ""
+
+# Opossum genome: Oct. 2004 (Broad prelim/monDom1)
+elif ref_genome == ""
+
+# Opossum genome: Jan. 2018 (Susie_PABv2/ponAbe3)
+elif ref_genome == ""
+
+# Opossum genome: Jul. 2007 (WUGSC 2.0.2/ponAbe2)
+elif ref_genome == ""
+
+# Painted turtle genome: Dec. 2011 (v3.0.1/chrPic1)
+elif ref_genome == ""
+
+# Panda genome: Dec. 2009 (BGI-Shenzhen 1.0/ailMel1)
+elif ref_genome == ""
+
+# Pig genome: Feb. 2017 (SGSC Sscrofa11.1/susScr11)
+elif ref_genome == ""
+
+# Pig genome: Aug. 2011 (SGSC Sscrofa10.2/susScr3)
+elif ref_genome == ""
+
+# Pig genome: Nov. 2009 (SGSC Sscrofa9.2/susScr2)
+elif ref_genome == ""
+
+# Pika genome: May 2012 (OchPri3.0/ochPri3)
+elif ref_genome == ""
+
+# Pika genome: Jul. 2008 (Broad/ochPri2)
+elif ref_genome == ""
+
+# Platypus genome: Feb. 2007 (ASM227v2/ornAna2)
+elif ref_genome == ""
+
+# Platypus genome: Mar. 2007 (WUGSC 5.0.1/ornAna1)
+elif ref_genome == ""
+
+# Proboscis monkey genome: Nov 2014 (Charlie1.0/nasLar1)
+elif ref_genome == ""
+
+# Rabbit genome: Apr. 2009 (Broad/oryCun2)
+elif ref_genome == ""
+
+# Rabbit genome: May 2005 (oryCun1)
+elif ref_genome == ""
+
+# Rat genome: Nov. 2020 (mRatBN7.2/rn7)
+elif ref_genome == ""
+
+# Rat genome: Jul. 2014 (RGSC 6.0/rn6)
+elif ref_genome == ""
+
+# Rat genome: Mar. 2012 (RGSC 5.0/rn5)
+elif ref_genome == ""
+
+# Rat genome: Nov. 2004 (Baylor 3.4/rn4)
+elif ref_genome == ""
+
+# Rat genome: Jun. 2003 (Baylor 3.1/rn3)
+elif ref_genome == ""
+
+# Rat genome: Jan. 2003 (rn2)
+elif ref_genome == ""
+
+# Rat genome: Nov. 2002 (rn1)
+elif ref_genome == ""
+
+# Rhesus genome: Feb. 2019 (Mmul_10/rheMac10)
+elif ref_genome == ""
+
+# Rhesus genome: Nov. 2015 (BCM Mmul_8.0.1/rheMac8)
+elif ref_genome == ""
+
+# Rhesus genome: Oct. 2010 (BGI CR_1.0/rheMac3)
+elif ref_genome == ""
+
+# Rhesus genome: Jan. 2006 (MGSC Merged 1.0/rheMac2)
+elif ref_genome == ""
+
+# Rhesus genome: Jan. 2005 (Mmul_0.1/rheMac1)
+elif ref_genome == ""
+
+# Rock hyrax genome: Jul. 2008 (Broad/proCap1)
+elif ref_genome == ""
+
+# Sheep genome: Nov. 2015 (ISGC Oar_v4.0/oviAri4)
+elif ref_genome == ""
+
+# Sheep genome: Aug. 2012 (ISGC Oar_v3.1/oviAri3)
+elif ref_genome == ""
+
+# Sheep genome: Feb. 2010 (ISGC Ovis_aries_1.0/oviAri1)
+elif ref_genome == ""
+
+# Shrew genome: Aug. 2008 (Broad/sorAra2)
+elif ref_genome == ""
+
+# Shrew genome: Jun. 2006 (Broad/sorAra1)
+elif ref_genome == ""
+
+# Sloth genome: Jul. 2008 (Broad/choHof1)
+elif ref_genome == ""
+
+# Southern sea otter genome: Jun. 2019 (ASM641071v1/enhLutNer1)
+elif ref_genome == ""
+
+# Squirrel genome: Nov. 2011 (Broad/speTri2)
+elif ref_genome == ""
+
+# Squirrel monkey genome: Oct. 2011 (Broad/saiBol1)
+elif ref_genome == ""
+
+# Stickleback genome: Feb. 2006 (Broad/gasAcu1)
+elif ref_genome == ""
+
+# Tarsier genome: Sep. 2013 (Tarsius_syrichta-2.0.1/tarSyr2)
+elif ref_genome == ""
+
+# Tarsier genome: Aug. 2008 (Broad/tarSyr1)
+elif ref_genome == ""
+
+# Tasmanian devil genome: Feb. 2011 (WTSI Devil_ref v7.0/sarHar1)
+elif ref_genome == ""
+
+# Tenrec genome: Nov. 2012 (Broad/echTel2)
+elif ref_genome == ""
+
+# Tenrec genome: Jul. 2005 (Broad/echTel1)
+elif ref_genome == ""
+
+# Tetraodon genome: Mar. 2010 (Genoscope 8.0/tetNig2)
+elif ref_genome == ""
+
+# Tetraodon genome: Feb. 2004 (Genoscope 7/tetNig1)
+elif ref_genome == ""
+
+# Tibetan frog genome: Mar. 2015 (BGI_ZX_2015/nanPar1)
+elif ref_genome == ""
+
+# Tree shrew genome: Dec. 2006 (Broad/tupBel1)
+elif ref_genome == ""
+
+# Turkey genome: Nov. 2014 (TGC Turkey_5.0/melGal5)
+elif ref_genome == ""
+
+# Turkey genome: Dec. 2009 (TGC Turkey_2.01/melGal1)
+elif ref_genome == ""
+
+# Wallaby genome: Sep. 2009 (TWGS Meug_1.1/macEug2)
+elif ref_genome == ""
+
+# White rhinoceros genome: May 2012 (CerSimSim1.0/cerSim1)
+elif ref_genome == ""
+
+# X. tropicalis genome: Jul. 2016 (JGI 9.1/xenTro9)
+elif ref_genome == ""
+
+# X. tropicalis genome: Sep. 2012 (JGI 7.0/xenTro7)
+elif ref_genome == ""
+
+# X. tropicalis genome: Nov. 2009 (JGI 4.2/xenTro3)
+elif ref_genome == ""
+
+# X. tropicalis genome: Aug. 2005 (JGI 4.1/xenTro2)
+elif ref_genome == ""
+
+# X. tropicalis genome: Oct. 2004 (JGI 3.0/xenTro1)
+elif ref_genome == ""
+
+# Zebra finch genome: Feb. 2013 (taeGut2/taeGut2)
+elif ref_genome == ""
+
+# Zebra finch genome: Jul. 2008 (WUGSC 3.2.4/taeGut1)
+elif ref_genome == ""
+
+# Zebrafish genome: May 2017 (GRCz11/danRer11)
+elif ref_genome == ""
+
+# Zebrafish genome: Sep. 2014 (GRCz10/danRer10)
+elif ref_genome == ""
+
+# Zebrafish genome: Jul. 2010 (Zv9/danRer7)
+elif ref_genome == ""
+
+# Zebrafish genome: Dec. 2008 (Zv8/danRer6) 
+elif ref_genome == ""
+
+# Zebrafish genome: Jul. 2007 (Zv7/danRer5)
+elif ref_genome == ""
+
+# Zebrafish genome: Mar. 2006 (Zv6/danRer4)
+elif ref_genome == ""
+
+# Zebrafish genome: May 2005 (Zv5/danRer3)
+elif ref_genome == ""
+
+# Zebrafish genome: Jun. 2004 (danRer2)
+elif ref_genome == ""
+
+# Zebrafish genome: Nov. 2003 (danRer1)
+elif ref_genome == ""
+
+
+## Invertebrate genomes 
+
+# Ciona intestinalis genome: Apr. 2011 (Kyoto KH/ci3)
+elif ref_genome == ""
+
+# Ciona intestinalis genome: Mar. 2005 (JGI 2.1/ci2)
+elif ref_genome == ""
+
+# Ciona intestinalis genome: Dec. 2002 (JGI 1.0/ci1)
+elif ref_genome == ""
+
+# S. purpuratus genome: Sep. 2006 (Baylor 2.1/strPur2)
+elif ref_genome == ""
+
+# S. purpuratus genome: Apr. 2005 (Baylor 1.1/strPur1)
+elif ref_genome == ""
+
+# A. gambiae genome: Nov. 2006 (ICSAGA P3/anoGam3)
+elif ref_genome == ""
+
+# A. gambiae genome: Feb. 2003 (IAGEC MOZ2/anoGam1)
+elif ref_genome == ""
+
+# A. mellifera genome: May 2005 (Baylor HGSC Amel_3.0/apiMel3)
+elif ref_genome == ""
+
+# A. mellifera genome: Jan. 2005 (Baylor 2.0/apiMel2)
+elif ref_genome == ""
+
+# A. mellifera genome: Jul. 2004 (Baylor 1.2/apiMel1)
+elif ref_genome == ""
+
+# D. ananassae genome: Feb. 2006 (droAna3)
+elif ref_genome == ""
+
+# D. ananassae genome: Aug. 2005 (Agencourt prelim/droAna2)
+elif ref_genome == ""
+
+# D. ananassae genome: Jul. 2004 (TIGR/droAna1 )
+elif ref_genome == ""
+
+# D. erecta genome: Feb. 2006 (droEre2)
+elif ref_genome == ""
+
+# D. erecta genome: Aug. 2005 (Agencourt prelim/droEre1)
+elif ref_genome == ""
+
+# D. grimshawi genome: Feb. 2006 (droGri2)
+elif ref_genome == ""
+
+# D. grimshawi genome: Aug. 2005 (Agencourt prelim/droGri1)
+elif ref_genome == ""
+
+# D. melanogaster genome: Aug. 2014 (BDGP Release 6 + ISO1 MT/dm6)
+elif ref_genome == ""
+
+# D. melanogaster genome: Apr. 2006 (BDGP R5/dm3)
+elif ref_genome == ""
+
+# D. melanogaster genome: Apr. 2006 (BDGP R5/dm3)
+elif ref_genome == ""
+
+# D. melanogaster genome: Jan. 2003 (BDGP R3/dm1)
+elif ref_genome == ""
+
+# D. mojavensis genome: Feb. 2006 (droMoj3)
+elif ref_genome == ""
+
+# D. mojavensis genome: Aug. 2005 (Agencourt prelim/droMoj2)
+elif ref_genome == ""
+
+# D. mojavensis genome: Aug. 2004 (Agencourt prelim/droMoj1)
+elif ref_genome == ""
+
+# D. persimilis genome: Oct. 2005 (Broad/droPer1)
+elif ref_genome == ""
+
+# D. pseudoobscura genome: Feb. 2006 (dp4)
+elif ref_genome == ""
+
+# D. pseudoobscura genome: Nov. 2004 (FlyBase 1.03/dp3)
+elif ref_genome == ""
+
+# D. pseudoobscura genome: Aug. 2003 (Baylor freeze1/dp2)
+elif ref_genome == ""
+
+# D. sechellia genome: Oct. 2005 (Broad/droSec1)
+elif ref_genome == ""
+
+# D. simulans genome: Apr. 2005 (WUGSC mosaic 1.0/droSim1)
+elif ref_genome == ""
+
+# D. virilis genome: Feb. 2006 (droVir3)
+elif ref_genome == ""
+
+# D. virilis genome: Aug. 2005 (Agencourt prelim/droVir2)
+elif ref_genome == ""
+
+# D. virilis genome: Jul. 2004 (Agencourt prelim/droVir1)
+elif ref_genome == ""
+
+# 
+elif ref_genome == ""
+
+# 
+elif ref_genome == ""
+
+# 
+elif ref_genome == ""
+
+# 
+elif ref_genome == ""
+
+# 
+elif ref_genome == ""
+
+# 
+elif ref_genome == ""
+
+# 
+elif ref_genome == ""
+
+# 
+elif ref_genome == ""
+
+# 
+elif ref_genome == ""
+
+# 
+elif ref_genome == ""
+
+# 
+elif ref_genome == ""
+
+# 
+elif ref_genome == ""
+
+# 
+elif ref_genome == ""
+
+# 
+elif ref_genome == ""
+
+# 
+elif ref_genome == ""
+
+# 
+elif ref_genome == ""
+
+# 
+elif ref_genome == ""
+
+# 
+elif ref_genome == ""
+
+# 
+elif ref_genome == ""
+
+# 
+elif ref_genome == ""
+
+# 
+elif ref_genome == ""
+
+# 
+elif ref_genome == ""
+
+# 
+elif ref_genome == ""
+
+# 
+elif ref_genome == ""
+
+# 
+elif ref_genome == ""
+
+# 
+elif ref_genome == ""
+
+# 
+elif ref_genome == ""
+
+# 
+elif ref_genome == ""
+
+# 
+elif ref_genome == ""
+
+# 
+elif ref_genome == ""
+# 
+elif ref_genome == ""
+
+# 
+elif ref_genome == ""
+
+# 
+elif ref_genome == ""
+
+# 
+elif ref_genome == ""
+
+# 
+elif ref_genome == ""
+
+# 
+elif ref_genome == ""
+
+# 
+elif ref_genome == ""
+
+# 
+elif ref_genome == ""
+
+# 
+elif ref_genome == ""
+
+# 
+elif ref_genome == ""
+
+# 
+elif ref_genome == ""
+
+# 
+elif ref_genome == ""
+
+
+rule download_genome:
+    message: "Downloading GRCm39/mm39 mouse genome from the UCSC Genome Browser"
+    output: "01_raw_data/mm39.chromFa.tar.gz"
+    log: "00_logs/download_genome.log"
+    shell: "wget https://hgdownload.soe.ucsc.edu/goldenPath/mm39/bigZips/mm39.chromFa.tar.gz -O {output} 2> {log}"
+
+rule process_genome:
+    message: "Decompressing genome. Concatenating individual chromosome files to create full assembly. Removing chromosome sequence files"
+    input: "01_raw_data/mm39.chromFa.tar.gz"
+    output: "01_raw_data/mm39.fa"
+    log: "00_logs/process_genome.log"
+    shell: """
+    tar zvfx {input} --directory 01_raw_data/
+    cat 01_raw_data/*.fa > {output} 2> {log}
+    rm 01_raw_data/chr*.fa
+    """
+    
+rule set_alignment_reference:
+    message: "Setting GRCm39/mm39 mouse genome assembly as reference genome for alignment" 
+    conda: "00_conda_software/chip_bwa.yml"
+    input: "01_raw_data/mm39.fa"
+    output: multiext("01_raw_data/mm39", ".amb", ".ann", ".bwt", ".pac", ".sa")
+    log: "00_logs/set_alignment_reference.log"
+    shell: """
+    bwa index -p mm39 -a bwtsw {input} 2> {log}
+    mv mm39* 01_raw_data/
+    """ 
