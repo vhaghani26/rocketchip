dd9ed29a05748abc1f69b5dd72be095994221b13
Haghani <vhaghani@ucdavis.edu>
2021-08-20 14:00:17 -0700

    work on getting secondary snakefile to work

--- under_construction/single_or_paired.py
+++ under_construction/single_or_paired.py
@@ -26,27 +26,36 @@ for link, sample in zip(links, config["samples"]):
         else:
             single_end.append(f'{sample}')
 
-# Add file name tags to distinguish paired and single end reads            
+# Check if 00_alignment_snakefiles/ exists and make it if it doesn't
+snakefile_dir = "00_alignment_snakefiles/"
+isExist = os.path.exists(snakefile_dir)
+if isExist == True:
+    print("The directory 00_alignment_snakefiles/ already exists")
+else:
+    os.system(f'mkdir 00_alignment_snakefiles')
+
+# Add file name tags to distinguish paired and single end reads     
 for sample in config["samples"]:
     if sample in paired_end:
         os.system(f'mv 01_raw_data/{sample}.html 01_raw_data/{sample}_paired.html')
-        os.system(f'touch {sample}_alignment_snakefile')
+        os.system(f'touch 00_alignment_snakefiles/{sample}_alignment_snakefile')
         print(f'{sample} is a paired-end read')
     else:
         os.system(f'mv 01_raw_data/{sample}.html 01_raw_data/{sample}_single.html')
-        os.system(f'touch {sample}_alignment_snakefile')
+        os.system(f'touch 00_alignment_snakefiles/{sample}_alignment_snakefile')
         print(f'{sample} is a single-end read')
-    with open(f'{sample}_alignment_snakefile', 'w') as fp:
+    with open(f'00_alignment_snakefiles/{sample}_alignment_snakefile', 'w') as fp:
         if sample in paired_end:
             paired_lines = [
                 'rule all:\n',
-                f'    "01_raw_data/{sample}_1.fastq.gz",\n',
-                f'    "01_raw_data/{sample}_2.fastq.gz",\n',
-                f'    "02_fastqc_analysis/{sample}_1_fastqc.html",\n',
-                f'    "02_fastqc_analysis/{sample}_1_fastqc.zip",\n',
-                f'    "02_fastqc_analysis/{sample}_2_fastqc.html",\n',
-                f'    "02_fastqc_analysis/{sample}_2_fastqc.zip",\n',
-                f'    "03_sam_files/{sample}.sam"\n',
+                '    input:\n',
+                f'        "01_raw_data/{sample}_1.fastq.gz",\n',
+                f'        "01_raw_data/{sample}_2.fastq.gz",\n',
+                f'        "02_fastqc_analysis/{sample}_1_fastqc.html",\n',
+                f'        "02_fastqc_analysis/{sample}_1_fastqc.zip",\n',
+                f'        "02_fastqc_analysis/{sample}_2_fastqc.html",\n',
+                f'        "02_fastqc_analysis/{sample}_2_fastqc.zip",\n',
+                f'        "03_sam_files/{sample}.sam"\n',
                 '\n',    
                 'rule sra_to_fastq_paired:\n',
                 '    message: "Converting SRA file to FASTQ file format"\n',
@@ -64,7 +73,6 @@ for sample in config["samples"]:
                 '    input:\n',
                 f'        r1 = "01_raw_data/{sample}_1.fastq.gz",\n',
                 f'        r2 = "01_raw_data/{sample}_2.fastq.gz",\n',
-                f'        dependency = "01_raw_data/{sample}_paired.html"\n',
                 '    output:\n',
                 f'        "02_fastqc_analysis/{sample}_1_fastqc.html",\n',
                 f'        "02_fastqc_analysis/{sample}_1_fastqc.zip",\n',
@@ -84,7 +92,6 @@ for sample in config["samples"]:
                 '    input:\n',
                 f'        r1 = "01_raw_data/{sample}_1.fastq.gz",\n',
                 f'        r2 = "01_raw_data/{sample}_2.fastq.gz",\n',
-                '        genome = multiext("01_raw_data/mm39", ".amb", ".ann", ".bwt", ".pac", ".sa")\n',
                 f'    output: "03_sam_files/{sample}.sam"\n',
                 f'    log: "00_logs/{sample}_align_reads_err.log"\n',
                 '    shell: "bwa mem 01_raw_data/mm39 {input.r1} {input.r2} > {output} 2> {log}"\n',
@@ -93,10 +100,11 @@ for sample in config["samples"]:
         else:
             single_lines = [
                 'rule all:\n',
-                f'    "01_raw_data/{sample}.fastq.gz",\n',
-                f'    "02_fastqc_analysis/{sample}_fastqc.html",\n',
-                f'    "02_fastqc_analysis/{sample}_fastqc.zip",\n',
-                f'    "03_sam_files/{sample}.sam",\n',
+                '    input:\n',
+                f'        "01_raw_data/{sample}.fastq.gz",\n',
+                f'        "02_fastqc_analysis/{sample}_fastqc.html",\n',
+                f'        "02_fastqc_analysis/{sample}_fastqc.zip",\n',
+                f'        "03_sam_files/{sample}.sam",\n',
                 '\n',
                 'rule sra_to_fastq_wc:\n',
                 '    message: "Converting SRA file to FASTQ file format"\n',
@@ -114,16 +122,14 @@ for sample in config["samples"]:
                 f'        "02_fastqc_analysis/{sample}_fastqc.html",\n',
                 f'        "02_fastqc_analysis/{sample}_fastqc.zip"\n',
                 f'    log: "00_logs/{sample}_fastqc_precheck_r1.log",\n',
-                '    shell: "fastqc {input.fastq} --outdir 02_fastqc_analysis/ 2> {log}"\n',
+                '    shell: "fastqc {input} --outdir 02_fastqc_analysis/ 2> {log}"\n',
                 '\n',
                 'rule align_reads_wc:\n',
                 '    message: "Aligning single end reads to GRCm39/mm39 reference genome"\n',
                 '    conda: "00_conda_software/chip_bwa.yml"\n',
-                '    input:\n',
-                f'        read = "01_raw_data/{sample}.fastq.gz",\n',
-                '        genome = multiext("01_raw_data/mm39", ".amb", ".ann", ".bwt", ".pac", ".sa"),\n',
+                f'    input: "01_raw_data/{sample}.fastq.gz",\n',
                 f'    output: "03_sam_files/{sample}.sam"\n',
                 f'    log: "00_logs/{sample}_align_reads_err.log"\n',
-                '    shell: "bwa mem 01_raw_data/mm39 {input.read} > {output} 2> {log}"\n',
+                '    shell: "bwa mem 01_raw_data/mm39 {input} > {output} 2> {log}"\n',
             ]
             fp.writelines(single_lines)
