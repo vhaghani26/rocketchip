8d38984d6f963f88fa00811aa752521fac94c8b4
Haghani <vhaghani@ucdavis.edu>
2021-08-20 11:14:27 -0700

    try fixing single and paired

--- under_construction/single_or_paired.py
+++ under_construction/single_or_paired.py
@@ -39,6 +39,15 @@ for sample in config["samples"]:
     with open(f'{sample}_alignment_snakefile', 'w') as fp:
         if sample in paired_end:
             paired_lines = [
+                'rule all:\n',
+                f'    "01_raw_data/{sample}_1.fastq.gz",\n',
+                f'    "01_raw_data/{sample}_2.fastq.gz",\n',
+                f'    "02_fastqc_analysis/{sample}_1_fastqc.html",\n',
+                f'    "02_fastqc_analysis/{sample}_1_fastqc.zip",\n',
+                f'    "02_fastqc_analysis/{sample}_2_fastqc.html",\n',
+                f'    "02_fastqc_analysis/{sample}_2_fastqc.zip",\n',
+                f'    "03_sam_files/{sample}.sam"\n',
+                '\n',    
                 'rule sra_to_fastq_paired:\n',
                 '    message: "Converting SRA file to FASTQ file format"\n',
                 '    conda: "00_conda_software/chip_sra.yml"\n',
@@ -82,4 +91,39 @@ for sample in config["samples"]:
             ]
             fp.writelines(paired_lines)
         else:
-            print(f'{sample} is a single-end read')
+            single_lines = [
+                'rule all:\n',
+                f'    "01_raw_data/{sample}.fastq.gz",\n',
+                f'    "02_fastqc_analysis/{sample}_fastqc.html",\n',
+                f'    "02_fastqc_analysis/{sample}_fastqc.zip",\n',
+                f'    "03_sam_files/{sample}.sam",\n',
+                '\n',
+                'rule sra_to_fastq_wc:\n',
+                '    message: "Converting SRA file to FASTQ file format"\n',
+                '    conda: "00_conda_software/chip_sra.yml"\n',
+                f'    input: "01_raw_data/{sample}/{sample}.sra"\n',
+                f'    output: "01_raw_data/{sample}.fastq.gz"\n',
+                f'    log: "00_logs/{sample}_sra_to_fastq.log"\n',
+                '    shell: "fastq-dump {input} --gzip --outdir 01_raw_data/ 2> {log}"\n',
+                '\n',
+                'rule fastqc_precheck_wc:\n',
+                '    message: "Running quality control on samples pre-processing"\n',
+                '    conda: "00_conda_software/chip_fastqc.yml"\n',
+                f'    input: "01_raw_data/{sample}.fastq.gz"\n',
+                '    output:\n',
+                f'        "02_fastqc_analysis/{sample}_fastqc.html",\n',
+                f'        "02_fastqc_analysis/{sample}_fastqc.zip"\n',
+                f'    log: "00_logs/{sample}_fastqc_precheck_r1.log",\n',
+                '    shell: "fastqc {input.fastq} --outdir 02_fastqc_analysis/ 2> {log}"\n',
+                '\n',
+                'rule align_reads_wc:\n',
+                '    message: "Aligning single end reads to GRCm39/mm39 reference genome"\n',
+                '    conda: "00_conda_software/chip_bwa.yml"\n',
+                '    input:\n',
+                f'        read = "01_raw_data/{sample}.fastq.gz",\n',
+                '        genome = multiext("01_raw_data/mm39", ".amb", ".ann", ".bwt", ".pac", ".sa"),\n',
+                f'    output: "03_sam_files/{sample}.sam"\n',
+                f'    log: "00_logs/{sample}_align_reads_err.log"\n',
+                '    shell: "bwa mem 01_raw_data/mm39 {input.read} > {output} 2> {log}"\n',
+            ]
+            fp.writelines(single_lines)
