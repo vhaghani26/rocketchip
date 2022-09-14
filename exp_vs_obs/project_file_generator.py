40adce78138263eb4954f1302e4423062abcbc78
Viktoria Haghani <vhaghani@ucdavis.edu>
2022-09-14 16:12:49 -0700

    condense

--- exp_vs_obs/project_file_generator.py
+++ exp_vs_obs/project_file_generator.py
@@ -12,97 +12,32 @@ for readtype in readtypes:
             for peakcaller in peakcallers:
                 for deduplicator in deduplicators:    
                     if readtype == "paired" and peaktype == "narrow":
-                        proj_file_info = f"""
-                        Author: Viktoria Haghani & Aditi Goyal
-                        Project: Exp_vs_Obs_{readtype}_{peaktype}_{aligner}_{peakcaller}_{deduplicator}
-                        Genome:
-                          Name: ref_genome_synth
-                          Location: 
-                        Reads:
-                          Samples:
-                            grp1: 
-                              - 
-                          Controls:
-                            ctl1: 
-                              - 
-                        Readtype: {readtype}
-                        Peaktype: {peaktype}
-                        Aligner: {aligner}
-                        Deduplicator: {deduplicator}
-                        Peakcaller: {peakcaller}
-                        Threads: 1
-                        """
+                        read_data_path = "a"
                     elif readtype == "paired" and peaktype == "broad":
-                        proj_file_info = f"""
-                        Author: Viktoria Haghani & Aditi Goyal
-                        Project: Exp_vs_Obs_{readtype}_{peaktype}_{aligner}_{peakcaller}_{deduplicator}
-                        Genome:
-                          Name: ref_genome_synth
-                          Location: 
-                        Reads:
-                          Samples:
-                            grp1: 
-                              - 
-                              - 
-                            grp2:
-                              - 
-                          Controls:
-                            ctl1: 
-                              - 
-                        Readtype: {readtype}
-                        Peaktype: {peaktype}
-                        Aligner: {aligner}
-                        Deduplicator: {deduplicator}
-                        Peakcaller: {peakcaller}
-                        Threads: 1
-                        """
+                        read_data_path = "b"
                     elif readtype == "single" and peaktype == "narrow":
-                        proj_file_info = f"""
-                        Author: Viktoria Haghani & Aditi Goyal
-                        Project: Exp_vs_Obs_{readtype}_{peaktype}_{aligner}_{peakcaller}_{deduplicator}
-                        Genome:
-                          Name: ref_genome_synth
-                          Location: 
-                        Reads:
-                          Samples:
-                            grp1: 
-                              - 
-                              - 
-                            grp2:
-                              - 
-                          Controls:
-                            ctl1: 
-                              - 
-                        Readtype: {readtype}
-                        Peaktype: {peaktype}
-                        Aligner: {aligner}
-                        Deduplicator: {deduplicator}
-                        Peakcaller: {peakcaller}
-                        Threads: 1
-                        """
+                        read_data_path = "c"
                     elif readtype == "single" and peaktype == "broad":
-                        proj_file_info = f"""
-                        Author: Viktoria Haghani & Aditi Goyal
-                        Project: Exp_vs_Obs_{readtype}_{peaktype}_{aligner}_{peakcaller}_{deduplicator}
-                        Genome:
-                          Name: ref_genome_synth
-                          Location: 
-                        Reads:
-                          Samples:
-                            grp1: 
-                              - 
-                              - 
-                            grp2:
-                              - 
-                          Controls:
-                            ctl1: 
-                              - 
-                        Readtype: {readtype}
-                        Peaktype: {peaktype}
-                        Aligner: {aligner}
-                        Deduplicator: {deduplicator}
-                        Peakcaller: {peakcaller}
-                        Threads: 1
-                        """
+                        read_data_path = "d"
+                    proj_file_info = f"""
+                    Author: Viktoria Haghani & Aditi Goyal
+                    Project: Exp_vs_Obs_{readtype}_{peaktype}_{aligner}_{peakcaller}_{deduplicator}
+                    Genome:
+                      Name: ref_genome_synth
+                      Location: 
+                    Reads:
+                      Samples:
+                        grp1: 
+                          - {read_data_path}
+                      Controls:
+                        ctl1: 
+                          - 
+                    Readtype: {readtype}
+                    Peaktype: {peaktype}
+                    Aligner: {aligner}
+                    Deduplicator: {deduplicator}
+                    Peakcaller: {peakcaller}
+                    Threads: 1
+                    """
                     print()
                     print(proj_file_info)
