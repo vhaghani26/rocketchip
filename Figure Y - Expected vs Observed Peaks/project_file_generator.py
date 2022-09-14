417f9465d866fd89d02efc204d21904db3fe46a7
Viktoria Haghani <vhaghani@ucdavis.edu>
2022-09-14 15:49:53 -0700

    waiting on generating data sets

--- Figure Y - Expected vs Observed Peaks/project_file_generator.py
+++ Figure Y - Expected vs Observed Peaks/project_file_generator.py
@@ -10,28 +10,102 @@ for readtype in readtypes:
     for peaktype in peaktypes:
         for aligner in aligners:
             for peakcaller in peakcallers:
-                for deduplicator in deduplicators:
-                    print(readtype, peaktype, aligner, peakcaller, deduplicator)
-
-
-
-
-"""
-print(f'Author: Viktoria Haghani & Aditi Goyal/n 
-        Project: Expected vs Observed Peaks/n
-        Genome:/n
-          Name: /n
-          Location: /n
-        Reads:/n
-          Samples:/n
-            grp1: /n
-            grp2:/n
-          Controls:/n
-            ctl1: /n
-        Readtype: /n
-        Peaktype: /n
-        Aligner: /n
-        Deduplicator: /n
-        Peakcaller: /n
-        Threads:')
-"""
+                for deduplicator in deduplicators:    
+                    if readtype == "paired" and peaktype == "narrow":
+                        proj_file_info = f"""
+                        Author: Viktoria Haghani & Aditi Goyal
+                        Project: Exp_vs_Obs_{readtype}_{peaktype}_{aligner}_{peakcaller}_{deduplicator}
+                        Genome:
+                          Name: 
+                          Location: 
+                        Reads:
+                          Samples:
+                            grp1: 
+                              - 
+                              - 
+                            grp2:
+                              - 
+                          Controls:
+                            ctl1: 
+                              - 
+                        Readtype: {readtype}
+                        Peaktype: {peaktype}
+                        Aligner: {aligner}
+                        Deduplicator: {deduplicator}
+                        Peakcaller: {peakcaller}
+                        Threads: 1
+                        """
+                    elif readtype == "paired" and peaktype == "broad":
+                        proj_file_info = f"""
+                        Author: Viktoria Haghani & Aditi Goyal
+                        Project: Exp_vs_Obs_{readtype}_{peaktype}_{aligner}_{peakcaller}_{deduplicator}
+                        Genome:
+                          Name: 
+                          Location: 
+                        Reads:
+                          Samples:
+                            grp1: 
+                              - 
+                              - 
+                            grp2:
+                              - 
+                          Controls:
+                            ctl1: 
+                              - 
+                        Readtype: {readtype}
+                        Peaktype: {peaktype}
+                        Aligner: {aligner}
+                        Deduplicator: {deduplicator}
+                        Peakcaller: {peakcaller}
+                        Threads: 1
+                        """
+                    elif readtype == "single" and peaktype == "narrow":
+                        proj_file_info = f"""
+                        Author: Viktoria Haghani & Aditi Goyal
+                        Project: Exp_vs_Obs_{readtype}_{peaktype}_{aligner}_{peakcaller}_{deduplicator}
+                        Genome:
+                          Name: 
+                          Location: 
+                        Reads:
+                          Samples:
+                            grp1: 
+                              - 
+                              - 
+                            grp2:
+                              - 
+                          Controls:
+                            ctl1: 
+                              - 
+                        Readtype: {readtype}
+                        Peaktype: {peaktype}
+                        Aligner: {aligner}
+                        Deduplicator: {deduplicator}
+                        Peakcaller: {peakcaller}
+                        Threads: 1
+                        """
+                    elif readtype == "single" and peaktype == "broad":
+                        proj_file_info = f"""
+                        Author: Viktoria Haghani & Aditi Goyal
+                        Project: Exp_vs_Obs_{readtype}_{peaktype}_{aligner}_{peakcaller}_{deduplicator}
+                        Genome:
+                          Name: 
+                          Location: 
+                        Reads:
+                          Samples:
+                            grp1: 
+                              - 
+                              - 
+                            grp2:
+                              - 
+                          Controls:
+                            ctl1: 
+                              - 
+                        Readtype: {readtype}
+                        Peaktype: {peaktype}
+                        Aligner: {aligner}
+                        Deduplicator: {deduplicator}
+                        Peakcaller: {peakcaller}
+                        Threads: 1
+                        """
+                    print()
+                    print(proj_file_info)
