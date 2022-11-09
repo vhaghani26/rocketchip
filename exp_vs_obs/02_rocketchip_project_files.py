1f77a542e06aac9310583a6382c79c6ae7eaac77
Viktoria Haghani <62615778+vhaghani26@users.noreply.github.com>
2022-11-08 16:15:30 -0800

    try troubleshooting rocketchip

--- exp_vs_obs/02_rocketchip_project_files.py
+++ exp_vs_obs/02_rocketchip_project_files.py
@@ -2,11 +2,16 @@
 
 import os
 
-readtypes = ["paired", "single"]
-peaktypes = ["narrow", "broad"]
-aligners = ["bwa_mem", "bowtie2", "STAR"]
-peakcallers = ["macs3", "cisgenome", "genrich", "pepr"]
-deduplicators = ["samtools", "no_deduplication", "sambamba", "picard"]
+'''
+Note: I ran this script in the main rocketchip directory using:
+    python3 exp_vs_obs/02_rocketchip_project_files.py
+'''
+
+readtypes = ["paired"]#, "single"]
+peaktypes = ["narrow"]#, "broad"]
+aligners = ["bwa_mem"]#, "bowtie2", "STAR"]
+peakcallers = ["macs3"]#, "cisgenome", "genrich", "pepr"]
+deduplicators = ["samtools"]#, "no_deduplication", "sambamba", "picard"]
 
 for readtype in readtypes:
     for peaktype in peaktypes:
@@ -14,5 +19,4 @@ for readtype in readtypes:
             for peakcaller in peakcallers:
                 for deduplicator in deduplicators:
                     for i in range(1,7):
-                        os.system(f'rocketchip ../exp_vs_obs/project_files/exp_vs_obs_{readtype}_{peaktype}_{aligner}_{peakcaller}_{deduplicator}_test_{i}.yaml --data ../exp_vs_obs/seq_data/{readtype}_{peaktype}/test_{i} --src .. --output-file ../exp_vs_obs/snakefiles/exp_vs_obs_{readtype}_{peaktype}_{aligner}_{peakcaller}_{deduplicator}_test_{i}')
-                        
+                        os.system(f'python3 rocketchip exp_vs_obs/project_files/exp_vs_obs_{readtype}_{peaktype}_{aligner}_{peakcaller}_{deduplicator}_test_{i}.yaml --data exp_vs_obs/seq_data/{readtype}_{peaktype}/test_{i} --src . --output-file exp_vs_obs/snakefiles/exp_vs_obs_{readtype}_{peaktype}_{aligner}_{peakcaller}_{deduplicator}_test_{i}')
