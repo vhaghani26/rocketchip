9052c51c2a49dbd337e8d0abdf68ec150062cff9
vhaghani26 <vhaghani@ucdavis.edu>
2022-11-10 10:42:57 -0800

    remake snakefiles with proper paths

--- exp_vs_obs/03_run_snakefiles.py
+++ exp_vs_obs/03_run_snakefiles.py
@@ -19,5 +19,10 @@ for readtype in readtypes:
             for peakcaller in peakcallers:
                 for deduplicator in deduplicators:
                     for i in range(1,7):
-                        # Dry run first
+                        # Change into snakefile directory
+                        os.chdir(f'')
+                        # Run snakefile
                         os.system(f'snakemake -j 4 -s exp_vs_obs/snakefiles/exp_vs_obs_{readtype}_{peaktype}_{aligner}_{peakcaller}_{deduplicator}_test_{i}/exp_vs_obs_{readtype}_{peaktype}_{aligner}_{peakcaller}_{deduplicator}_test_{i} > exp_vs_obs/snakefiles/exp_vs_obs_{readtype}_{peaktype}_{aligner}_{peakcaller}_{deduplicator}_test_{i}/')
+                        # Go back to original directory
+                        os.chdir(f'')
+                        break
