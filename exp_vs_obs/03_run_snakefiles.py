b5a4bc0e31d7d6f92b92d114d93f673847cde2df
vhaghani26 <vhaghani@ucdavis.edu>
2022-11-10 11:11:52 -0800

    determine issue with snakefiles

--- exp_vs_obs/03_run_snakefiles.py
+++ exp_vs_obs/03_run_snakefiles.py
@@ -20,9 +20,11 @@ for readtype in readtypes:
                 for deduplicator in deduplicators:
                     for i in range(1,7):
                         # Change into snakefile directory
-                        os.chdir(f'exp_vs_obs/snakefiles/exp_vs_obs_{readtype}_{peaktype}_{aligner}_{peakcaller}_{deduplicator}_test_{i}/')
+                        os.chdir(f'exp_vs_obs/snakefiles/exp_vs_obs_{readtype}_{peaktype}_{aligner}_{peakcaller}_{deduplicator}_test_{i}')
+                        os.system('pwd')
+                        os.system('ls')
                         # Run snakefile
-                        os.system(f'snakemake -j 4 -s exp_vs_obs/snakefiles/exp_vs_obs_{readtype}_{peaktype}_{aligner}_{peakcaller}_{deduplicator}_test_{i}/exp_vs_obs_{readtype}_{peaktype}_{aligner}_{peakcaller}_{deduplicator}_test_{i}')
+                        os.system(f'snakemake -j 4 -s exp_vs_obs_{readtype}_{peaktype}_{aligner}_{peakcaller}_{deduplicator}_test_{i}')
                         # Go back to original directory
                         os.chdir(f'../../../')
                         break
